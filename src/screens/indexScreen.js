0493f3a601786a1e169e4196197c031de1fdc46d
alemarnery <aleemar.95@gmail.com>
2021-01-28 12:00:58 -0430

    BlogContext

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,10 +1,20 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
+  const value = useContext(BlogContext);
   return (
     <View>
       <Text>Index Screen</Text>
+      <FlatList
+        data={blogPosts}
+        keyExtractor={(blogPosts) => blogPosts.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      ></FlatList>
+      <Text>{value}</Text>
     </View>
   );
 };
