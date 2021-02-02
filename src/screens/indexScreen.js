cc0a33851e9f292e776ff63a3c64cc3adb21636a
alemarnery <aleemar.95@gmail.com>
2021-02-02 10:48:14 -0430

    blogContext

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,12 +1,13 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
-  const value = useContext(BlogContext);
+  const { data, addBlogPost } = useContext(BlogContext);
   return (
     <View>
       <Text>Index Screen</Text>
+      <Buttton title="Add Post" onPress={addBlogPost} />
       <FlatList
         data={blogPosts}
         keyExtractor={(blogPosts) => blogPosts.title}
